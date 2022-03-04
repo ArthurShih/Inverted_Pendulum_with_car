488292b2732b483b06a87ab496c936bac08a1e25
ArthurShih <arthurshih93@gmail.com>
2022-03-04 12:28:12 -0800

    initial commit

---
+++ Parameters.m
@@ -0,0 +1,6 @@
+mc = 10; % mass of car
+mp = 0.1; % mass of pendulum
+l = 1; % length of pendulum
+ell = l/2; % center of pendulum
+I = m*ell^2/12; % moment of inerita of pendulum
+g = 9.81; % gravity
